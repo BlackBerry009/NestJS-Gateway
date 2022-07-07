f7f3f0b665d7b48e6bd6295f9dbba8cf118584f8
Artist M <baymax97@163.com>
2022-07-07 23:19:25 +0800

    Replace express2fastify

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { VersioningType, VERSION_NEUTRAL } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: [VERSION_NEUTRAL, '1', '2']
+  })
+
   await app.listen(3000);
 }
+
 bootstrap();
